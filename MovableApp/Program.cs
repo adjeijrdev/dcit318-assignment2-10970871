6d63e284f6231662f1bc5e7a95e24c7d782d130d
Daniel <adjeijuniordaniel@gmail.com>
2024-06-17 00:51:21 +0000

    first commit

---
+++ MovableApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+interface IMovable
+{
+    void Move();
+}
+
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IMovable car = new Car();
+        IMovable bicycle = new Bicycle();
+
+        car.Move();        // Outputs: Car is moving
+        bicycle.Move();    // Outputs: Bicycle is moving
+    }
+}
